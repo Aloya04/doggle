faab015c5258ae0d5593058a3d2c97598d44837c
Aloya04 <alicjabal.edu@gmail.com>
2023-12-15 02:44:05 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
+// This JavaScript code changes the document title to display a custom message ("Milo Misses You!" 
+//when the user switches tabs or loses focus, 
+//and restores the original title when the user returns to the tab.
+
+
+// Get the current document title
 let title = document.title;
+
+// Add an event listener for when the window loses focus (e.g., user switches tabs)
 window.addEventListener("blur", () => {
+    // Change the document title to a custom message when the window loses focus
     document.title = "Milo Misses You!";
-})
+});
+
+// Add an event listener for when the window gains focus again
 window.addEventListener("focus", () => {
+    // Restore the original document title when the window regains focus
     document.title = title;
-})
+});
+
+
+//This JavaScript code creates a sticky header that becomes fixed at the top of the page when the user scrolls down, 
+//and returns to its normal position when scrolling back up.
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
